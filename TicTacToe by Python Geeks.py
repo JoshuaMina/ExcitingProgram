afe5f07ee2f691511f667ae7ca07d72ebca48d1f
JoshuaMina <joshua.minacambronero@gmail.com>
2022-11-19 17:13:14 +0800

    Initialize game window

--- TicTacToe by Python Geeks.py
+++ TicTacToe by Python Geeks.py
@@ -34,3 +34,19 @@ def drawlines():
     pygame.draw.line(Screen, colorofline, (0, 400), (600, 400), 10)
     pygame.draw.line(Screen, colorofline, (200, 0), (200, 600), 10)
     pygame.draw.line(Screen, colorofline, (400, 0), (400, 600), 10)
+
+
+def drawfigures():
+    for col in range(Columns):
+        for row in range(Rows):
+            if Board[row][col] == 1:
+                pygame.draw.circle(Screen, colorofcircle, (
+                int(col * sizeofsquare + sizeofsquare / 2), int(row * sizeofsquare + sizeofsquare / 2)), radiusofcircle,
+                                   circlelinewidth)
+            elif Board[row][col] == 2:
+                pygame.draw.line(Screen, xcolor, (col * sizeofsquare + offset, row * sizeofsquare + offset), (
+                col * sizeofsquare + sizeofsquare - offset, row * sizeofsquare + sizeofsquare - offset), xlinewidth)
+                pygame.draw.line(Screen, xcolor,
+                                 (col * sizeofsquare + offset, row * sizeofsquare + sizeofsquare - offset),
+                                 (col * sizeofsquare + sizeofsquare - offset, row * sizeofsquare + offset), xlinewidth)
+
