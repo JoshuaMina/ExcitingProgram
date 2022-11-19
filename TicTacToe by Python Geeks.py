0766191e0cd9f97122dd8e444e7cd18de570802a
JoshuaMina <joshua.minacambronero@gmail.com>
2022-11-19 17:03:20 +0800

    define variables and color

--- TicTacToe by Python Geeks.py
+++ TicTacToe by Python Geeks.py
@@ -3,3 +3,20 @@ import pygame
 import math
 import sys
 
+Rows = 3
+Columns = 3
+sizeofsquare = 200
+radiusofcircle = 60
+offset = 55
+circlelinewidth= 15
+xlinewidth = 25
+widthofscreen = Columns * sizeofsquare
+heightofscreen = Rows * sizeofsquare
+colorofline = (0,0,0)
+backgroundcolour = (4,0,127)
+colorofcircle= (255,255,0)
+xcolor = (255,0,0)
+player = 0
+gameover = False
+inmenu = True
+
